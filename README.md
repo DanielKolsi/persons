394524c32540f29a8887aac97f7810e8a3944085
Daniel Agape Kolsi <kolsi.daniel@gmail.com>
2017-06-30 18:23:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+Persons
+=======
+
+This app lists person data in an HTML table. It's implemented using Javascript and the Node
+framework. JQuery is used for AJAX & manipulating HTML elements. CSS is used for styling.
+
+
+========================
+installation:
+========================
+
+After closing / downloading from GitHub. Cd to the main folder.
+Execute the following commands:
+
+1) npm install
+2) npm start
+3) browse to: http://127.0.0.1/3000
+
+========================
+Assumptions
+========================
+As this was a quick asked to be returned the next day, unit & functional tests were
+omitted.
+
+Also, the code contains some 'hard coded' values which could be outsourced to a separate config
+file if there were more time.
+
+Generally, the assignment's functionality and UI should match the design exactly or as well as
+possible.
+
+Names are in ascending alphabetical order according to the full name.
+
+As the assignment didn't exactly mention where to output the names when "Confirm" was clicked,
+the names will be outputted both to browser's console.log & to alert dialog.
+
+-- Daniel Kolsi (kolsi.daniel@gmail.com)
---
+++ static/js/persons.js
@@ -0,0 +1,125 @@
+// global properties
+var Properties = (function() {
+	var module = {};
+	module.API = "https://jsonplaceholder.typicode.com/users"; // API URL for fetching the JSON user data
+	module.names = [];
+	module.emails = [];
+	module.names_emails_hash = new Object(); // for getting the correct email for sorted names list
+	module.selectedNames = []; // checkbox was checked on these names (rows)
+	module.numberOfSelectedNames = 0;
+	module.PERSONS_BODY = '#personsTable tbody';
+	return module;
+}());
+
+(function() {
+	$(document).ready(function() {
+		Get.fetchPersonsBatch(); // fetch person data immediately when the document is ready & loaded
+	});
+})();
+
+
+// output the selected names both in console & in alert dialog
+var OutputSelected = (function() {
+
+	var module = {};
+
+	module.output = function() {
+
+		var selectedNames = '';
+
+		for (var i in Properties.selectedNames) {
+
+			var name = Properties.selectedNames[i];
+
+			if (name != null && typeof name !== "undefined") {
+				console.log(name);
+				selectedNames = selectedNames + '\n' + name;
+			}
+		}
+		if (Properties.numberOfSelectedNames > 0) {
+			alert(selectedNames);
+		} else {
+			alert('No names were selected.');
+		}
+
+	};
+	return module;
+}());
+
+// REST Get for fetching data from the third party API
+var Get = (function() {
+
+	var module = {};
+
+	/*
+	AJAX function to fetch the data from the API (url)
+	*/
+	module.fetchPersonsBatch = function() {
+
+		$.ajax({
+			url: Properties.API,
+			type: 'GET',
+			dataType: "json",
+			success: this.processData,
+			error: this.error,
+		});
+	};
+
+
+	module.processData = function(data) {
+
+		$.each(data, function(idx) {
+			var name = data[idx].name;
+			var email = data[idx].email;
+			Properties.names[idx] = name;
+			Properties.emails[idx] = email;
+			Properties.names_emails_hash[name] = email; // create hash for preserving order of names/emails after sorting
+		});
+
+		Properties.names.sort(); // names need to be sorted in ASCENDING order before displaying & sort(); this default sorting does it right!
+
+		$.each(data, function(idx) {
+
+			var name = Properties.names[idx];
+			var email = Properties.names_emails_hash[name]; // get the correct email (after ASC name sort) from the hash!
+
+			var nameRow = '<tr class="name"><td style="text-align:center; background-color:#D9D9D9; border: none" rowspan = "2"><input id="' + idx +
+				'"type="checkbox" style="background-color: white" onclick="handleClick(this)" /></td><td style="text-align:left; background-color:#E9E9E9">' + name + '</td></tr>';
+			$(nameRow).appendTo(Properties.PERSONS_BODY);
+			var emailRow = '<tr class="email">' + '<td style="text-align:left; background-color:#FFFFFF">' + email + '</td>' + '</tr>';
+			$(emailRow).appendTo(Properties.PERSONS_BODY);
+			var emptyRow = '<tr class="space"><td colspan="2"></td></tr>';
+			$(emptyRow).appendTo(Properties.PERSONS_BODY);
+		});
+	};
+
+	module.error = function(jqXHR, exception) {
+		if (jqXHR.status && jqXHR.status == 400) {
+			alert(jqXHR.responseText);
+		} else {
+			alert("Error while fetching JSON data.");
+		}
+	};
+	return module;
+}());
+
+
+// if checkBox was checked, add corresponding name to the selectedNames array otherwise assign null to the array
+function handleClick(cb) {
+	$('.select').css({
+		fontSize: 18
+	});
+
+	if (cb.checked) {
+		Properties.selectedNames[cb.id] = Properties.names[cb.id];
+		Properties.numberOfSelectedNames++;
+	} else {
+		Properties.selectedNames[cb.id] = null; // remove the element from the array
+		Properties.numberOfSelectedNames--;
+	}
+	if (Properties.numberOfSelectedNames > 0) {
+		$('.select').text('' + Properties.numberOfSelectedNames + ' of ' + Properties.names.length + ' selected');
+	} else {
+		$('.select').text('');
+	}
+}
