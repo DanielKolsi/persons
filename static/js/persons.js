84ef99e6d3d528da0959ef0a9e9497ef193402b7
Daniel Agape Kolsi <kolsi.daniel@gmail.com>
2017-06-30 19:40:31 -0500

    minor fixes

--- static/js/persons.js
+++ static/js/persons.js
@@ -86,10 +86,7 @@ var Get = (function() {
 			var nameRow = '<tr class="name"><td style="text-align:center; background-color:#D9D9D9; border: none" rowspan = "2"><input id="' + idx +
 				'"type="checkbox" style="background-color: white" onclick="handleClick(this)" /></td><td style="text-align:left; background-color:#E9E9E9">' + name + '</td></tr>';
 			$(nameRow).appendTo(Properties.PERSONS_BODY);
-			var emailRow = "<tr class="
-			email ">" + "<td style="
-			text - align: left;
-			background - color: #FFFFFF ">" + email + "</td>" + "</tr>";
+			var emailRow = "<tr class="email ">" + "<td style="text - align: left;background - color: #FFFFFF ">" + email + "</td>" + "</tr>";
 			$(emailRow).appendTo(Properties.PERSONS_BODY);
 
 			var emptyRow = "<tr class="space"><td colspan="2"></td></tr>";
